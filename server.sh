f38d16ba1e37f06a7a00655aa8dc07cee963ccef
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-09-14 08:14:59 -0300

    Setup MongoDB with EVE for RESTful API.

--- server.sh
+++ server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+echo "Stopping any running Python processes."
+killall python
 echo "Starting MongoDB"
-mongod >> ./logs/mongod.log &
+mongod &
 echo "Starting Eve"
-python server/server.py >> ./logs/eve.log &
+python server/server.py &
 echo "Starting Python Simple HTTP Server"
 ip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 # echo "Your local IP address is $ip"
--- server/settings.py
+++ server/settings.py
@@ -1,14 +1,55 @@
 SERVER_NAME = '127.0.0.1:5000'
+
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH) and deletes of individual items
+# (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+# We enable standard client cache directives for all resources exposed by the
+# API. We can always override these global settings later.
+CACHE_CONTROL = 'max-age=20'
+CACHE_EXPIRES = 20
+
+# Our API will expose two resources (MongoDB collections): 
+bus_stops = {
+    # if 'item_title' is not provided Eve will just strip the final
+    # 's' from resource name, and use it as the item_title.
+    'item_title': 'Bus_Stops',
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    'schema': {
+        'title': {
+            'type': 'string',
+            'required': True,
+        },
+        'description': {
+            'type': 'string',
+        }
+    }
+}
+
+# The DOMAIN dict explains which resources will be available and how they will
+# be accessible to the API consumer.
 DOMAIN = {
-    'people': {},
+    'bus_stops': bus_stops
 }
 
 # Let's just use the local mongod instance. Edit as needed.
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
+#MONGO_HOST = 'localhost'
+#MONGO_PORT = 27017
 MONGO_USERNAME = 'glavin'
-MONGO_PASSWORD = 'rule'
+MONGO_PASSWORD = 'rules'
 MONGO_DBNAME = 'test'
+X_DOMAINS = "*"
+X_HEADERS = "*"
