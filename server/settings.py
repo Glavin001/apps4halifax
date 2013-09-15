25dafa5ec340137542d80689ad030197104c664e
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-09-15 11:53:33 -0300

    Fixed schema.

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ DOMAIN = {
     'Halifax' : {
         
         'schema': {
-        	'_SocrataId': {'type': 'string', 'unique': True, 'required': True },
+        	'_SocrataID': {'type': 'string', 'unique': True, 'required': True },
             'type': { 'type': 'string', 'required': True },
             'longitude': { 'type': 'float', 'required': True },
             'latitude': { 'type': 'float', 'required': True },
